8b032670c4453f37f851f6d88d324617830d83ca
sherwin <sherwinnikolaisaga@gmail.com>
2023-12-18 15:33:11 +0800

    Add project files.

---
+++ PropagationActivity/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace PropagationActivity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(15, 58);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(15, 101);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Create NN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(169, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Train";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(311, 208);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Test";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(15, 139);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(299, 79);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 7;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(15, 332);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(104, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Save Weights";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(299, 335);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 10;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(159, 332);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(85, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Load Weights";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(308, 319);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Training Iterations";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 375);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PropagationActivity/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Backprop;
+
+namespace PropagationActivity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ctr = 0;
+        }
+
+        NeuralNet nn;
+        int ctr;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nn = new NeuralNet(4,1,1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int n = 100;
+            for(int i = 0; i < n; i++)
+            {
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 0.0);
+                nn.setInputs(2, 0.0);
+                nn.setInputs(3, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 0.0);
+                nn.setInputs(2, 0.0);
+                nn.setInputs(3, 1.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 0.0);
+                nn.setInputs(2, 1.0);
+                nn.setInputs(3, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 0.0);
+                nn.setInputs(2, 1.0);
+                nn.setInputs(3, 1.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 1.0);
+                nn.setInputs(2, 0.0);
+                nn.setInputs(3, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 1.0);
+                nn.setInputs(2, 0.0);
+                nn.setInputs(3, 1.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 1.0);
+                nn.setInputs(2, 1.0);
+                nn.setInputs(3, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 1.0);
+                nn.setInputs(2, 1.0);
+                nn.setInputs(3, 1.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 0.0);
+                nn.setInputs(2, 0.0);
+                nn.setInputs(3, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 0.0);
+                nn.setInputs(2, 0.0);
+                nn.setInputs(3, 1.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 0.0);
+                nn.setInputs(2, 1.0);
+                nn.setInputs(3, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 0.0);
+                nn.setInputs(2, 1.0);
+                nn.setInputs(3, 1.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 1.0);
+                nn.setInputs(2, 0.0);
+                nn.setInputs(3, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 1.0);
+                nn.setInputs(2, 0.0);
+                nn.setInputs(3, 1.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 1.0);
+                nn.setInputs(2, 1.0);
+                nn.setInputs(3, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 1.0);
+                nn.setInputs(2, 1.0);
+                nn.setInputs(3, 1.0);
+                nn.setDesiredOutput(0, 1.0);
+                nn.learn();
+
+            }
+            ctr += n;
+            textBox7.Text = "" + ctr;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            nn.setInputs(0, Convert.ToDouble(textBox1.Text));
+            nn.setInputs(1, Convert.ToDouble(textBox2.Text));
+            nn.setInputs(2, Convert.ToDouble(textBox3.Text));
+            nn.setInputs(3, Convert.ToDouble(textBox4.Text));
+            nn.run();
+            textBox5.Text = ""+nn.getOuputData(0);
+
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            if (nn != null)
+            {
+                nn.saveWeights(saveFileDialog1.FileName);
+            }
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            if (nn != null)
+            {
+                String path = openFileDialog1.FileName;
+                nn.loadWeights(path);
+            }
+            else
+            {
+                MessageBox.Show("Object not instantiated");
+            }
+        }
+    }
+}
